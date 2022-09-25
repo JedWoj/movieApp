f20d3acf81399260171212be3355bbbf86fee887
JedWoj <jedwojc@gmail.com>
2022-09-25 17:33:09 +0200

    beta version of nav added

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,10 +1,25 @@
-import { Container } from '@chakra-ui/react';
+import { Box, Container, Flex, Heading } from '@chakra-ui/react';
+import FilmwebLogo from '../../public/images/FilmwebLogo.png';
+import Link from 'next/link';
+import Image from 'next/image';
 import React from 'react'
 
 const Nav = () => {
     return(
-        <Container maxWidth={'container.xl'}>
-            Jestem
+        <Container minWidth={'full'}>
+            <Flex alignItems={'center'}>
+                <Link href={'/'}> 
+                    <Image src={FilmwebLogo} height={120} width={240} alt='filmweb logo' />
+                </Link>
+                <Flex fontSize={25} marginRight={12} gap={20} flex={1} justifyContent={'flex-end'}>
+                    <Link href={'/films'}>
+                        Films
+                    </Link>
+                    <Link href={'/serials'}>
+                        Serials
+                    </Link>
+                </Flex>
+            </Flex>
         </Container>
     )
 }
