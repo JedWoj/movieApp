bc0e6a83b770810165c361d334222c8c3a8aec57
JedWoj <jedwojc@gmail.com>
2022-10-23 22:30:47 +0200

    FilmsLitsItem implemented - WIP

--- components/FilmsList/FilmsListItem.tsx
+++ components/FilmsList/FilmsListItem.tsx
@@ -1,6 +1,8 @@
-import { ListItem, Box } from '@chakra-ui/react';
+import { ListItem, Box, Flex, Heading } from '@chakra-ui/react';
+import Image from 'next/image';
+import Link from 'next/link';
 import { SingleGenreMovie } from '../../types/genreMovies';
-import Card from '../UI/Card';
+import Card from '../UI/Card'; 
 
 interface FilmsListItemType {
     film: SingleGenreMovie,
@@ -8,13 +10,20 @@ interface FilmsListItemType {
 
 const FilmsListItem = ({film}: FilmsListItemType) => {
     return(
-        <ListItem>
-            <Card>
-                <Box>
-                    {film.overview}
-                </Box>
-            </Card>
-        </ListItem>)
+        <Link href={`films/${film.id}`}>
+            <ListItem>
+                <Card>
+                    <Flex>
+                        <Box flexBasis={'100'}>
+                            <Image src={`https://image.tmdb.org/t/p/w500${film.poster_path}`} alt={`poster of ${film.title}`} width={'80'} height={'100'} />
+                        </Box>
+                        <Heading fontSize={{base: 'md', sm: 'lg', xl: '2xl'}}>
+                            {film.title}
+                        </Heading>
+                    </Flex>
+                </Card>
+            </ListItem>
+        </Link>)
 }
 
 export default FilmsListItem;
